be47eb0a74d77d095fb4528c62c2d11caf17f413
Frantz Chery <frantzchery@hotmail.com>
2024-05-29 10:37:24 -0400

    Added color borders

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header.jsx';
 import SearchBar from './components/SearchBar.jsx';
 import EmployeeList from './components/EmployeeList.jsx';
 
+
+
 function App() {
   return (
     <div className="app">
